32e13629b09973c662e0e745addf7da7410ed14b
Sarim <sarimyx@proton.me>
2025-12-21 06:19:41 -0700

    feat: add sandbox stripe payment urls for dev env

--- config/offerings.ts
+++ config/offerings.ts
@@ -10,20 +10,20 @@ export const PKWY_OFFERINGS: {
             description: "One-time Women-Only Pilates Sculpt class. No commitment.",
             classes: 1,
             price_per_class: 15,
-            stripe_payment_link: "https://buy.stripe.com/cNi5kC9vfc0qc4c9eZ3AY03"
+            stripe_payment_link: process.env.NODE_ENV === "production" ? "https://buy.stripe.com/cNi5kC9vfc0qc4c9eZ3AY03" : "https://buy.stripe.com/test_14A7sK6ig2kn6Nh9GA1Fe01"
         },
         {
             title: "Monthly Membership",
             description: "Includes 4 Women-Only Pilates Sculpt classes each month.",
             classes: 4,
             price_per_class: 20,
-            stripe_payment_link: "https://buy.stripe.com/test_8x26oG7n76G64BK62N3AY01"
+            stripe_payment_link: process.env.NODE_ENV === "production" ? "https://buy.stripe.com/8x26oG7n76G64BK62N3AY0176G64BK62N3AY01" : "https://buy.stripe.com/test_8x25kCgWU0cf7Rl0601Fe02"
         },
         {
             title: "Friendship Membership",
             description: "One membership for two women. 4 Pilates Sculpt classes each month.",
             classes: 4,
             price_per_class: 30,
-            stripe_payment_link: "https://buy.stripe.com/test_8x228q8rbggGeckezj3AY02"
+            stripe_payment_link: process.env.NODE_ENV === "production" ? "https://buy.stripe.com/8x228q8rbggGeckezj3AY02" : "https://buy.stripe.com/test_7sYcN45ec3or5Jd1a41Fe03"
         }
     ];
