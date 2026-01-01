76e875782476a46048a9fc14ea23fa42bc615485
Sarim <sarimyx@proton.me>
2025-12-31 17:33:08 -0700

    fix: proper env key

--- config/offerings.ts
+++ config/offerings.ts
@@ -18,7 +18,7 @@ export const PKWY_OFFERINGS: {
             description: "A weekly movement ritual centered on strength, breath, and alignment. Includes four classes per month and resets monthly.",
             classes: 4,
             price_per_class: 15,
-            stripe_payment_link: process.env.VERCEL_ENV === "pkwywellness.com" || process.env.VERCEL_ENV === "www.pkwywellness.com"
+            stripe_payment_link: process.env.VERCEL_URL === "pkwywellness.com" || process.env.VERCEL_URL === "www.pkwywellness.com"
                 ? "https://buy.stripe.com/4gM28q5eZ2pQfgo62N3AY05"
                 : "https://buy.stripe.com/test_cNi5kC9vfc0qc4c9eZ3AY03"
         },
@@ -27,7 +27,7 @@ export const PKWY_OFFERINGS: {
             description: "One membership shared between two women moving together. Includes four total classes per month to use together or separately.",
             classes: 4,
             price_per_class: 27.5,
-            stripe_payment_link: process.env.VERCEL_ENV === "pkwywellness.com" || process.env.VERCEL_ENV === "www.pkwywellness.com"
+            stripe_payment_link: process.env.VERCEL_URL === "pkwywellness.com" || process.env.VERCEL_URL === "www.pkwywellness.com"
                 ? "https://buy.stripe.com/6oU14mfTD1lMd8gdvf3AY04"
                 : "https://buy.stripe.com/test_8x228q8rbggGeckezj3AY02"
         },
